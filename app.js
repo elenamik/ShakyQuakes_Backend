ab53c1db833590ac9857efa08dce1bbee5da3ecc
Elena Mikhaylova <Lena@Mac.lan>
2019-07-27 10:48:42 -0400

    refactoring

--- app.js
+++ app.js
@@ -4,25 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser=require('body-parser')
-const session = require('express-session')
 const mongoose=require('mongoose');
-require('./schema/LatLongData');
+require('./schema/LatLongData'); //schema included before routes
 var cors =require('cors')
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var locateRouter = require('./routes/locate');
 
 express.static('/')
+require('dotenv').config({ path: 'config/variables.env' });
 
-const MongoStore = require('connect-mongo')(session);
 mongoose.Promise = global.Promise;
-
 const options = {
   useNewUrlParser: true,
 }
 
-const mongoURL="mongodb://lena:shaky2019@ds215089.mlab.com:15089/shaky-quakes"
-
-mongoose.connect(process.env.DATABASE || mongoURL, options)
+mongoose.connect(process.env.MONGO_URL, options)
 .then(
   ()=> {console.log("connected to MongoDB")},
   (err)=>{console.log(err);}
@@ -33,20 +29,12 @@ mongoose.connection.on('disconnected', () => { console.log('-> lost connection')
 // The driver tries to automatically reconnect by default, so when the
 // server starts the driver will reconnect and emit a 'reconnect' event.
 mongoose.connection.on('reconnect', () => { console.log('-> reconnected'); });
-
 // Mongoose will also emit a 'connected' event along with 'reconnect'. These
 // events are interchangeable.
 mongoose.connection.on('connected', () => { console.log('-> connected'); });
 
 
-
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -57,26 +45,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-//app.use('/uploads',express.static('uploads'))
+
 
 // launch backend into a port
-const API_PORT=5000;
+const API_PORT=process.env.API_PORT
 const server=app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
-//const server=app.listen(process.env.PORT || API_PORT)
-
-module.exports = app;
-
-
 const database = mongoose.connection;
-app.use(session({
-  secret: process.env.SECRET || "SECRETSESH",
-  resave: false,
-  saveUninitialized: true,
-  store: new MongoStore({ mongooseConnection: database })
-}));
 
+
+//routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/locate', locateRouter);
+
 app.use(express.static('public'))
 
 // catch 404 and forward to error handler
---
+++ routes/locate.js
@@ -0,0 +1,144 @@
+var express = require('express');
+var router = express.Router();
+let {PythonShell} = require('python-shell')
+const mongoose=require('mongoose');
+mongoose.Promise = global.Promise;
+const LatLongData = mongoose.model('LatLongData');
+
+
+router.get('/populateLocations', function(req,res,next){
+  const base=[{lat:-25.000,lng:133.000},{lat:-34.000,lng:150.000},{lat:29.000,lng:84.000},{lat:32.000,lng:117.000}]
+
+  const regions=Number(req.query.regions) || 1
+  const points=Number(req.query.points) || 1
+  const data_points=[]
+  console.log(`generating ${points} points for ${regions} regions`)
+
+  for (let i=1; i<=regions; i++){
+    index=Math.floor(Math.random() * base.length )//need it to be between 0 and length
+    const basePoint=base[index]
+  
+    for (let j=1; j<=points ;j++){
+      // lat
+      const b1=basePoint.lat
+      const sign1=Math.pow(-1,2) //(-1) to power of random number between 1 and 2
+      const distance1=Math.random()/10 //random .01 decimal
+      const lat=b1+sign1*distance1
+    
+      // //lng
+      const b2=basePoint.lng
+      const sign2=Math.pow(-1,2) //(-1) to power of random number between 1 and 2
+      const distance2=Math.random()/10 //random .01 decimal
+      const lng=b2+sign2*distance2
+     
+      //ts
+      var today = new Date();
+      var date = String(today.getFullYear())+'-'+(padZero(today.getMonth()+1))+'-'+padZero(today.getDate());
+      var time = padZero(today.getHours()) + ":" + padZero(today.getMinutes()) + ":" + padZero(today.getSeconds());
+      var timestamp= date+' '+time;
+
+      //photo
+      //filename should get generated randomly and stored in memory. for now we only have 3 photos
+      const filenum=String(Math.floor(Math.random() * 2 +1))
+      const filename=`human${filenum}.jpg`
+      console.log('FILE NAME',filename)
+
+      const point=new LatLongData({
+        location:{
+          lat,
+          lng
+        },
+        timestamp,
+        photo:filename
+      });
+        data_points.push(point)
+      }
+  }
+  console.log(data_points)
+  LatLongData.collection.insertMany(data_points)
+  .then(()=>{
+      res.send({
+          success:true,
+          message:"data added successfully"
+      })
+  })
+  .catch(err =>{
+      console.log(err);
+      res.send(
+          {sucess:false,
+            message:'data did not add to DB'
+          }
+      );
+  });
+})
+
+
+router.get('/getLocations',function(req,res){
+  console.log("")
+  const lowerBound=req.query.lowerBound;
+  const upperBound=req.query.upperBound;
+  console.log(`querying location points for results between ${lowerBound} and ${upperBound}`)
+  LatLongData.find(
+    {
+      timestamp:{
+        $gte:lowerBound,
+        $lte:upperBound
+      }
+    }
+  ).then(results=>{
+    console.log(results)
+    res.send(
+      {success:true,
+        locations:results
+    })
+  })
+  .catch(err=>{
+    console.log(err)
+    res.send({
+      success:false,
+      message:"coult not fetch from DB"
+    })
+  })
+})
+
+router.get('/checkForHuman', function(req,res,next){
+  const imgName='human2.jpg'
+  //const imgName=req.body.imageName
+
+  let options = {
+    mode: 'text',
+    pythonPath: 'venv/bin/python3',
+    scriptPath: 'bin',
+    pythonOptions: ['-u'],
+    args: [imgName]
+  }
+    console.log(`Running analysis on ${imgName}`)
+
+     PythonShell.run('human_detection_model.py', options, function (err, results) {
+      if (err) {
+        console.log(err)
+        res.send({
+          success:false,
+          error:err
+        })
+      }
+      // Results is an array consisting of messages collected during execution
+      console.log('results: %j', results)
+      res.send({success:true,
+        results})
+    })
+
+   
+})
+
+
+function padZero(value){
+  if (value<=9){
+      value=`0${value}`
+  }
+  return value;
+}
+ 
+
+module.exports = router;
+
